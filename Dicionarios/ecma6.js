410a08ff36e5090c084c2fe270a60a229ee37233
Davi Firmino <dafirsouza@gmail.com>
2018-03-01 03:38:46 +0000

    ES6 Dictionary

--- Dicionarios/ecma6.js
+++ Dicionarios/ecma6.js
@@ -1,4 +1,8 @@
-//O ecma2015 implementou a classe Map como parte de sua api para a criação de dicionarios
+// O ecma2015 implementou a classe Map como parte de sua api para a criação de dicionarios
+// Junto com a classe Map tambem foi criado as Classes WeakMap e WeakSet Ambas as classes possuem Tipo Fraco
+// E apenas Objetos podem ser usados como chave
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/WeakMap
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/WeakSet
 var map = new Map()
 map.set('gandalf','gandalf@terrordasnovinhas.com')
 map.set('batman','batman@marta.com')
